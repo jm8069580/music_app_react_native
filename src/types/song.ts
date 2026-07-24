0a3d3bff713bd3925b21011563397dd534c19934
juan.quiroz <jq8069580@gmail.com>
2026-07-24 13:46:20 -0500

    feat(types): add video_uri field to Song type

--- src/types/song.ts
+++ src/types/song.ts
@@ -11,9 +11,10 @@ export type Song = {
   metadata_extracted: number; // 0 | 1
   is_favorite: number; // 0 | 1
   lyrics: string | null;
+  video_uri: string | null;
 };
 
-export type NewSong = Omit<Song, 'id' | 'metadata_extracted' | 'is_favorite' | 'lyrics'>;
+export type NewSong = Omit<Song, 'id' | 'metadata_extracted' | 'is_favorite' | 'lyrics' | 'video_uri'>;
 
 export type SongMetadataUpdate = {
   title?: string;
